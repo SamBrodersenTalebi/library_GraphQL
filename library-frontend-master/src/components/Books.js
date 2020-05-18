09ce32cdc38cfc9168989235de9fe6eec8cf61b9
Sam Brodersen <sam.talebi84@gmail.com>
2020-05-18 12:14:22 +0200

    book view fetch all books

--- library-frontend-master/src/components/Books.js
+++ library-frontend-master/src/components/Books.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from 'react';
+import { useQuery } from '@apollo/client';
+import { ALL_BOOKS } from '../services/queries';
 
 const Books = (props) => {
-  if (!props.show) {
-    return null
-  }
+  const result = useQuery(ALL_BOOKS);
 
-  const books = []
+  //if loading is true return loading
+  if (result.loading) return <div>loading</div>;
+
+  if (!props.show) {
+    return null;
+  }
 
   return (
     <div>
@@ -15,24 +20,20 @@ const Books = (props) => {
         <tbody>
           <tr>
             <th></th>
-            <th>
-              author
-            </th>
-            <th>
-              published
-            </th>
+            <th>author</th>
+            <th>published</th>
           </tr>
-          {books.map(a =>
+          {result.data.allBooks.map((a) => (
             <tr key={a.title}>
               <td>{a.title}</td>
               <td>{a.author}</td>
               <td>{a.published}</td>
             </tr>
-          )}
+          ))}
         </tbody>
       </table>
     </div>
-  )
-}
+  );
+};
 
-export default Books
+export default Books;
